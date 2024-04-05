c638c806cfd12c62062e93d0bc65de4f740406c7
gaozhengxin <zhengxingao@live.cn>
2024-04-06 06:26:47 +0800

    add receiveMessage, getEvmMessage

--- app/relay.ts
+++ app/relay.ts
@@ -85,6 +85,7 @@ const main = async () => {
   console.log(pdas);
 
   // 1. Create RelayData account
+  /*
   const createRelayDataTx = await valueRouterProgram.methods
     .createRelayData()
     // eventAuthority and program accounts are implicitly added by Anchor
@@ -98,8 +99,10 @@ const main = async () => {
     .rpc();
 
   console.log("createRelayDataTx: ", createRelayDataTx);
+  */
 
   // 2. Post bridge data
+  /*
   const postBridgeMessageTx = await valueRouterProgram.methods
     .postBridgeMessage({
       bridgeMessage: {
@@ -115,6 +118,7 @@ const main = async () => {
     .rpc();
 
   console.log("postBridgeMessageTx: ", postBridgeMessageTx);
+  */
 
   // 3. Post swap data
   /*
@@ -221,7 +225,10 @@ const main = async () => {
       usedNonces: pdas.usedNonces1,
       systemProgram: SystemProgram.programId,
       messageTransmitterEventAuthority: eventAuthority,
-      relayParams: relayDataKeypair.publicKey,
+      //relayParams: relayDataKeypair.publicKey,
+      relayParams: new PublicKey(
+        "Ee7jv9pPPWWzKRnqf5Kw1d1Lp7vtApTGmcbTu4WnDYVP"
+      ),
       tokenMessengerMinterProgram: tokenMessengerMinterProgram.programId,
       tokenProgram: TOKEN_PROGRAM_ID,
       usdcVault: userTokenAccount,
