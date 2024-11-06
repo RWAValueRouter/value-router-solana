5d8dc822ce303af25d97c28082e313c05f28da52
dmsoltech <freshtopromise@gmail.com>
2024-11-06 22:08:46 +0300

    update: check status with ErrorCode using require! instead of assert!

--- programs/value-router/src/errors.rs
+++ programs/value-router/src/errors.rs
@@ -18,4 +18,21 @@ pub enum ErrorCode {
     CctpReceiverMismatch,
     #[msg("invalid bump seed provided")]
     InvalidBump,
+    #[msg("Wrong Fee Receiver")]
+    WrongFeeReceiver,
+    #[msg("insufficient balance to swap")]
+    NotEnoughBalance,
+    #[msg("invalid swap version")]
+    WrongSwapVersion,
+    #[msg("mismatch source domain")]
+    WrongSourceDomain,
+    #[msg("mismatch nonce")]
+    WrongNonce,
+    #[msg("mismatch recipient's wallet account")]
+    WrongRecipientWallet,
+    #[msg("mismatch recipient's output token account")]
+    WrongRecipientTokenAccount,
+    #[msg("mismatch recipient's usdc account")],
+    WrongRecipientUSDCAccount,
+
 }
